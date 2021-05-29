6ee446ba014923dc85e55d28ebe47f032e111e05
omerozturk18 <46124907+omerozturk18@users.noreply.github.com>
2021-05-29 10:53:57 +0300

    Add files via upload

---
+++ src/main/java/omerozturk/hrms/business/abstracts/JobPostingService.java
@@ -0,0 +1,17 @@
+package omerozturk.hrms.business.abstracts;
+
+import omerozturk.hrms.core.utilities.result.DataResult;
+import omerozturk.hrms.core.utilities.result.Result;
+import omerozturk.hrms.entities.concretes.JobPosting;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JobPostingService {
+	Result add(JobPosting jobPosting);
+	Result changeJobPostingStatus(int jobPostingId);
+	DataResult<JobPosting> getByJobPostingId(int jobPostingId);
+	DataResult<List<JobPosting>> getByActiveJobPostings();
+	DataResult<List<JobPosting>> getByReleaseDateJobPosting(int value);
+	DataResult<List<JobPosting>> getByEmployerJobPosting(int employerId);
+}
