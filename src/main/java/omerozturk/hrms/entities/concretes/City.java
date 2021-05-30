8184e69e9b450999a957d8bae2d378346eee4302
omerozturk18 <46124907+omerozturk18@users.noreply.github.com>
2021-05-30 20:06:53 +0300

    Create City.java

---
+++ src/main/java/omerozturk/hrms/entities/concretes/City.java
@@ -0,0 +1,27 @@
+package omerozturk.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","jobPostings"})
+@Table(name="cities")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "city")
+    private List<JobPosting> jobPostings;
+}
